8ae5ac4c101f2c7046cf89a0c5f6e0e2f1170fb1
FWQX | ST40L <137301764+ST40l@users.noreply.github.com>
2023-08-12 15:35:15 +0300

    Update st40l.py

--- st40l.py
+++ st40l.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-il = input("Bir il girin: ").lower()  # Kullanıcının girdiği ili küçük harfe çeviriyoruz
+il = input("Bir il girin: ").lower()
 
 url = f"https://www.harita.gov.tr/iller/{il}.html"
 response = requests.get(url)
@@ -9,18 +9,23 @@ response = requests.get(url)
 if response.status_code == 200:
     soup = BeautifulSoup(response.content, "html.parser")
     
-    internet_sayisi = soup.find("div", class_="InternetSayisi").get_text()
-    turksat = "Türksat" in internet_sayisi
-    turktelekom = "Türk Telekom" in internet_sayisi
-    turkcell = "Türkcell" in internet_sayisi
+    internet_sayisi = soup.find("div", class_="InternetSayisi")
     
-    print(f"{il.capitalize()} ilindeki internet sayısı: {internet_sayisi}")
-    
-    if turksat:
-        print("Türksat internet hizmeti mevcut.")
-    if turktelekom:
-        print("Türk Telekom internet hizmeti mevcut.")
-    if turkcell:
-        print("Turkcell internet hizmeti mevcut.")
+    if internet_sayisi:
+        internet_sayisi = internet_sayisi.get_text()
+        turksat = "Türksat" in internet_sayisi
+        turktelekom = "Türk Telekom" in internet_sayisi
+        turkcell = "Turkcell" in internet_sayisi
+        
+        print(f"{il.capitalize()} ilindeki internet sayısı: {internet_sayisi}")
+        
+        if turksat:
+            print("Türksat internet hizmeti mevcut.")
+        if turktelekom:
+            print("Türk Telekom internet hizmeti mevcut.")
+        if turkcell:
+            print("Turkcell internet hizmeti mevcut.")
+    else:
+        print("İnternet sayısı bilgisi bulunamadı.")
 else:
     print("İl bulunamadı veya sayfaya erişim sağlanamadı.")
