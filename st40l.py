61eb6613ff90690d5ae926ab7fdb0e3c4b35f71c
FWQX | ST40L <137301764+ST40l@users.noreply.github.com>
2023-08-12 15:36:22 +0300

    Update st40l.py

--- st40l.py
+++ st40l.py
@@ -13,18 +13,22 @@ if response.status_code == 200:
     
     if internet_sayisi:
         internet_sayisi = internet_sayisi.get_text()
-        turksat = "Türksat" in internet_sayisi
-        turktelekom = "Türk Telekom" in internet_sayisi
-        turkcell = "Turkcell" in internet_sayisi
         
-        print(f"{il.capitalize()} ilindeki internet sayısı: {internet_sayisi}")
-        
-        if turksat:
-            print("Türksat internet hizmeti mevcut.")
-        if turktelekom:
-            print("Türk Telekom internet hizmeti mevcut.")
-        if turkcell:
-            print("Turkcell internet hizmeti mevcut.")
+        if "İnternet" in internet_sayisi:
+            turksat = "Türksat" in internet_sayisi
+            turktelekom = "Türk Telekom" in internet_sayisi
+            turkcell = "Turkcell" in internet_sayisi
+            
+            print(f"{il.capitalize()} ilindeki internet sayısı: {internet_sayisi}")
+            
+            if turksat:
+                print("Türksat internet hizmeti mevcut.")
+            if turktelekom:
+                print("Türk Telekom internet hizmeti mevcut.")
+            if turkcell:
+                print("Turkcell internet hizmeti mevcut.")
+        else:
+            print("İnternet sayısı bilgisi bulunamadı.")
     else:
         print("İnternet sayısı bilgisi bulunamadı.")
 else:
