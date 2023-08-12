23c353acc5c16f32dbfce985a9b33235a7566f49
FWQX | ST40L <137301764+ST40l@users.noreply.github.com>
2023-08-12 15:27:42 +0300

    Update st40l.py

--- st40l.py
+++ st40l.py
@@ -1,19 +1,47 @@
 import requests
-from bs4 import BeautifulSoup
 
-def get_internet_data(il):
-    url = f"https://www.harita.gov.tr/sunum/{il}"
-    response = requests.get(url)
+def get_data(api_url, params):
+    response = requests.get(api_url, params=params)
 
     if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-        # İnternet sayfasındaki ilgili veriyi burada çekebilirsiniz
-        # Örnek olarak başlığı çekelim:
-        title = soup.title.text
-        return title
+        data = response.json()  # Gelen veriyi JSON formatından Python nesnesine çeviriyoruz
+        return data
     else:
-        return f"Sayfaya erişilemiyor. Hata kodu: {response.status_code}"
+        return None
 
 il = input("Bir il adı girin: ")
-veri = get_internet_data(il)
-print("İnternet Verisi:", veri)
+url = f"https://www.harita.gov.tr/sunum/{il}"
+
+# İnternet verisi için istek
+internet_params = {
+    "il": il
+}
+internet_veri = get_data(api_url, internet_params)
+
+if internet_veri:
+    internet_degeri = internet_veri.get("internet_verisi")
+    
+    if internet_degeri is not None:
+        print(f"{il} ilindeki internet verisi: {internet_degeri}")
+    else:
+        print(f"{il} iline ait internet verisi bulunamadı.")
+else:
+    print(f"{il} için internet verisi çekilemedi veya hata oluştu.")
+
+# IP adresleri için istek
+ip_params = {
+    "il": il
+}
+ip_veri = get_data(api_url, ip_params)
+
+if ip_veri:
+    ip_listesi = ip_veri.get("ip_list")
+    
+    if ip_listesi:
+        print(f"{il} iline ait IP adresleri:")
+        for ip in ip_listesi:
+            print(ip)
+    else:
+        print(f"{il} iline ait IP adresi bilgisi bulunamadı.")
+else:
+    print(f"{il} için IP adresi verisi çekilemedi veya hata oluştu.")
